4683dd072125bdb60ecd3f18d95cc35fa8c281bb
KetanM9555 <134033159+KetanM9555@users.noreply.github.com>
2024-02-06 18:58:52 +0530

    some minor cganges

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use(
 app.use(
   cors({
     credentials: true,
-    origin: process.env.FRONTEND_URI,
+    // origin: process.env.FRONTEND_URI,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
