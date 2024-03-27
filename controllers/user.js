87e2ffc407b7aac12a180cf0ab4348713f0f2ee6
KetanM9555 <134033159+KetanM9555@users.noreply.github.com>
2024-03-27 13:39:43 +0530

    add the frontend url

--- controllers/user.js
+++ controllers/user.js
@@ -13,9 +13,9 @@ export const logout = (req, res, next) => {
   req.session.destroy((err) => {
     if (err) return next(err);
     res.clearCookie("connect.sid", {
-      // secure: process.env.NODE_ENV === "development" ? false : true,
-      // httpOnly: process.env.NODE_ENV === "development" ? false : true,
-      // sameSite: process.env.NODE_ENV === "development" ? false : "none",
+      secure: process.env.NODE_ENV === "development" ? false : true,
+      httpOnly: process.env.NODE_ENV === "development" ? false : true,
+      sameSite: process.env.NODE_ENV === "development" ? false : "none",
       secure: true,
       httpOnly: true,
       sameSite: "none",
